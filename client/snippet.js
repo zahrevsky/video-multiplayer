3b69d739df99a3e7c1479616e632f7c08564be28
Vladyslav Zagrevskiy <vzvl.dos@gmail.com>
2022-03-15 20:09:42 +0200

    Fix server from localhost to vzvlad.dev

--- client/snippet.js
+++ client/snippet.js
@@ -1,17 +1,17 @@
 var USE_FETCH = false
 
-let ws = new WebSocket("ws://localhost:8080/shared-video-control/subscribe")
+let ws = new WebSocket("wss://vzvlad.dev:8080/shared-video-control/subscribe")
 let video = document.getElementsByTagName('video')[0]
 
 video.onplay = async (event) => {
 	if (USE_FETCH)
-		fetch('http://localhost:8080/shared-video-control/play', {method: 'POST'})
+		fetch('https://vzvlad.dev:8080/shared-video-control/play', {method: 'POST'})
 	else
 		ws.send("play")
 }
 video.onpause = async (event) => {
 	if (USE_FETCH)
-		fetch('http://localhost:8080/shared-video-control/pause', {method: 'POST'})
+		fetch('https://vzvlad.dev:8080/shared-video-control/pause', {method: 'POST'})
 	else
 		ws.send("pause")
 }
